42dffdc81cf438552af671b5484971110e8ded39
ArfaNaufal <85439610+ArfaNaufal@users.noreply.github.com>
2022-08-01 11:21:38 +0700

    Update formRegistrasi.dart

--- perhitungan_bangundatar/lib/formRegistrasi.dart
+++ perhitungan_bangundatar/lib/formRegistrasi.dart
@@ -20,90 +20,92 @@ class _formRegistrasiState extends State<formRegistrasi> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Form Registrasi'),),
-      body: Container(
-        margin: EdgeInsets.all(10),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            TextField(
-              controller: ctrUsername,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Username',
+      body: SingleChildScrollView(
+        child: Container(
+          margin: EdgeInsets.all(10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextField(
+                controller: ctrUsername,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Username',
+                ),
               ),
-            ),
-            Container(height: 20,),
-            TextField(
-              controller: ctrPassword,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Password',
+              Container(height: 20,),
+              TextField(
+                controller: ctrPassword,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Password',
+                ),
               ),
-            ),
-            SizedBox(height: 20,),
-            Text("pilih jenis kelamin", style: TextStyle(fontSize: 15),),
-            SizedBox(height: 10,),
-            Row(
-              children: [
-                Radio(
-                  value: 1, 
-                  groupValue: id, 
-                  onChanged: (val) {
+              SizedBox(height: 20,),
+              Text("pilih jenis kelamin", style: TextStyle(fontSize: 15),),
+              SizedBox(height: 10,),
+              Row(
+                children: [
+                  Radio(
+                    value: 1, 
+                    groupValue: id, 
+                    onChanged: (val) {
+                      setState(() {
+                        id= 1;
+                      });
+                      
+      
+                    }
+                  ),
+                  Text("male")
+                ],
+              ),
+               Row(
+                children: [
+                  Radio(
+                    value: 2, 
+                    groupValue: id, 
+                    onChanged: (val) {
+                      setState(() {
+                        id= 2;
+                      });
+      
+                    }
+                  ),
+                  Text("female")
+                ],
+               ),
+               Text("phone :"+ NomerHangpong),
+               ElevatedButton(
+                onPressed:() async{
+                  final result = await
+                  Navigator.push(
+                      context, 
+                      MaterialPageRoute(builder: (context) => const SecondScreen()),
+                    );
                     setState(() {
-                      id= 1;
+                      NomerHangpong = result.toString();
                     });
+               },child: Text("Phone Number")),
+               
+               SizedBox(height: 30,),
+              
+            
+      
+              
+              Center(
+                child: Container(
+                  width: 200,
+                  child: ElevatedButton(onPressed: (){
                     
-
-                  }
+                  }, child: Text("Submit")),
                 ),
-                Text("male")
-              ],
-            ),
-             Row(
-              children: [
-                Radio(
-                  value: 2, 
-                  groupValue: id, 
-                  onChanged: (val) {
-                    setState(() {
-                      id= 2;
-                    });
-
-                  }
-                ),
-                Text("female")
-              ],
-             ),
-             Text("phone :"+ NomerHangpong),
-             ElevatedButton(
-              onPressed:() async{
-                final result = await
-                Navigator.push(
-                    context, 
-                    MaterialPageRoute(builder: (context) => const SecondScreen()),
-                  );
-                  setState(() {
-                    NomerHangpong = result.toString();
-                  });
-             },child: Text("Phone Number")),
-             
-             SizedBox(height: 30,),
+              )
+            ],
             
-          
-
             
-            Center(
-              child: Container(
-                width: 200,
-                child: ElevatedButton(onPressed: (){
-                  
-                }, child: Text("Submit")),
-              ),
-            )
-          ],
-          
-          
-          
+            
+          ),
         ),
       ),
     );
